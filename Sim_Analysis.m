111ef82f9239b10106c2cd42149e70b650832836
Lianys Feliciano <106699241+lianysfeliciano@users.noreply.github.com>
2023-06-14 15:54:18 +0200

    Add files via upload

--- Sim_Analysis.m
+++ Sim_Analysis.m
@@ -5,25 +5,46 @@ addpath(path)
 %[p_times,p_freqs,p_eq_ps,xgrid,kgrid,color_grid,found] = experiment_multi_sigs...
     %(N, minf,maxf,mc,white_noise,amp,plot_flag,save_fig,where_am_i )
 
- 
-%
-i=1; 
-p_times =zeros(4);
-p_freqs =zeros(4);
-p_eq_ps =zeros(4);
-xgrind =zeros(4);
-color_grind =zeros(4);
-found =zeros(4);
-
-while i<4  
+%for i=1:4  
     
-    [times,freqs,p_eq_ps,xgrid,kgrid,color_grid,found]=example_multi_sigs(2,6,20,1.5,1,1e-22,1,1,'laptop');
-    i=i+1;
-    p_timee{i}=times;
-    p_freqs{i}=freqs;
-    xgrind{i}=xgrind;
-    kgrind{i}=kgrind;
-    color_grind{i}=color_grind
-    found{i}=found
+    %example_multi_sigs(); 
 
-end 
+%end 
+
+%Input Parameters
+Num =1:10;
+whitenoise=1;
+minfreq=zeros(10,1); %Making these empty arrays to fill with all the same value | When varyinng these will be input arrays 
+maxfreq=zeros(10,1);
+chirpm=zeros(10,1);
+amplitute=zeros(10,1);
+donde='laptop';
+
+%Filling Empty Input Arrays
+
+minfreq(:)=3;
+maxfreq(:)=19;
+chirpm(:)=2.5;
+amplitute(:)=1e-25;
+
+
+%Creating empty arrays for outputs
+registered_sig= zeros(10,1);
+%tfft= zeros(10,1);
+
+
+for i=1:length(Num)
+    
+    %Simulating
+    [p_times,p_freqs,p_eq_ps,xgrid,kgrid,color_grid,found]=experiment_multi_sigs(Num(i),minfreq,maxfreq,chirpm,whitenoise,amplitute,1,0,donde);
+    
+    %Filling output array with output vals
+    registered_sig(i)=found;
+    % tfft(i)=TFFTmax;
+    
+end
+
+%Storing everything into a df
+Sim_df(10,Num,registered_sig,tfft,minffreq,maxfreq,amplitute,chirpm)
+
+
--- Sim_df.m
+++ Sim_df.m
@@ -1,63 +1,50 @@
-function [df] =Sim_df(Name,Loc, N,Retireved_sig,Params,TFFT)
+function [df] =Sim_df(Num_sims, n,Retireved_sig,tfft,minfreq,maxfreq,amps,chirpm)
 
 %Sim_df
 %This function creates and stores a dafa frame of a hough simulation
 %   This function is intended to store values of many simulations at once. 
 %   By implimenting this function within a loop of simulations it will
-%   store every indivisual loop
-%  
-%Inputs
-%------------------------------------------------------------------------
-% Variable Name        Definition
+%   store every loop
 % 
-% Name                 Name of Dataframe
-% Loc                  Desiered Location of Dataframe
-% Num_sims             Number of simulations run 
-%  N                   Number of injected signals
-% Retrived_sig         Number of retireved signals 
-% Params               Array of Input Parameters (minf,maxf,Mc,white_noise, )
-% TFFT                 Total number of TFFT's
-% 
-%
-% -----------------------------------------------------------------------------
-%
-%Outputs
 %-----------------------------------------------------------------------
-% Variable Name        Definition
+%               
 % df                   Dataframe with all input values
 %
+%df is separated by commas and has a header of one row. No idx col. 
 %-------------------------------------------------------------------------------
-
-%Setting default values of input if none are provided. 
-
-Name= ("/df_"+ N +"_signals");
-
-Loc= "~/Nikhef/Output_Code";
 
 
 %Creating empty arrays for each column
-if N_sims>1
-    N=zeros(Num_sims);
-    Retireved_sig=zeros(Num_sims);
-    TFFT=zeros(Num_sims);
+
+ N=zeros(Num_sims,1);
+ Retireved_N=zeros(Num_sims,1); 
+ TFFT=zeros(Num_sims,1);
+ minf=zeros(Num_sims,1);
+ maxf=zeros(Num_sims,1);
+ amp=zeros(Num_sims,1);
+ cm=zeros(Num_sims,1);
+
+
+
+
+%Insertinng values into empty arrays
+for i = 1:Num_sims
+    N(i)=n(i);
+    Retireved_N(i)=Retireved_sig(i); 
+    TFFT(i)=tfft(i);
+    minf(i)= minfreq(i);
+    maxf(i)=maxfreq(i); 
+    amp(i)=amps(i);
+    cm(i)=chirpm(i);
+
+
 end
 
 
-%Insertinng values into 
-for i = 1:N_sims
-    N{i}=
-    Retireved_sig{i}=
-    TFFT{i}=
+%Composing tabel of values
+df=table(N,Retireved_N,TFFT,minf,maxf,amp,cm);
 
-end
-
-df=tabel(N,Retireved_sig,Params,TFFT);
-
-saveas(df,Loc+Name+".csvread")
-
-
-
-
-
+%Saving df as csv file 
+writetable(df,'df.csv');
 
 end
