3df2e77f276b0b385c5aab722fe95cb692e4c20d
Lianys Feliciano <106699241+lianysfeliciano@users.noreply.github.com>
2023-06-30 09:14:41 +0200

    Add files via upload

--- Sim_Analysis.m
+++ Sim_Analysis.m
@@ -4,47 +4,91 @@ addpath(path)
 
 %[p_times,p_freqs,p_eq_ps,xgrid,kgrid,color_grid,found] = experiment_multi_sigs...
     %(N, minf,maxf,mc,white_noise,amp,plot_flag,save_fig,where_am_i )
-
+    
 %for i=1:4  
     
     %example_multi_sigs(); 
 
 %end 
 
-%Input Parameters
-Num =1:10;
+%Number of Sims
+Sims=50; 
+
+%Innput Parameters
+ 
+%For random values withinn range use this: xmin+rand(1,n)*(xmax-xmin)
+Num =[1,2,3,4,5,6,7,8,9,10];
 whitenoise=1;
-minfreq=zeros(10,1); %Making these empty arrays to fill with all the same value | When varyinng these will be input arrays 
-maxfreq=zeros(10,1);
-chirpm=zeros(10,1);
-amplitute=zeros(10,1);
+minfreq=zeros(Sims,1);
+maxfreq=zeros(Sims,1);
+chirpm=zeros(Sims,1);
+amplitute=[1e-22,1e-23,1e-24];
+
+
+%minfreq=randi([2,4],Sims,1);      % Array of random (integer) values between 2 and 4 for min frequency (Must be whole numbers
+%maxfreq=randi([6,8],Sims,1);      
+%chirpm=0.5+rand(Sims,1)*(3-0.5);
+
 donde='laptop';
 
 %Filling Empty Input Arrays
-
-minfreq(:)=3;
-maxfreq(:)=19;
-chirpm(:)=2.5;
-amplitute(:)=1e-25;
+minfreq(:)=4;
+maxfreq(:)=7;
+chirpm(:)= 1.15;
 
 
 %Creating empty arrays for outputs
-registered_sig= zeros(10,1);
-%tfft= zeros(10,1);
+registered_sig= zeros(Sims,1);
+tfft= zeros(Sims,1);
+Dur=zeros(Sims,1);
+Cr=zeros(Sims,1);
+DistAway=zeros(Sims,1);
+F0=zeros(Sims,1);
+amp=zeros(Sims,1);
+
+%Initialiting tables 
 
 
-for i=1:length(Num)
+
+for n=1:10  
     
-    %Simulating
-    [p_times,p_freqs,p_eq_ps,xgrid,kgrid,color_grid,found]=experiment_multi_sigs(Num(i),minfreq,maxfreq,chirpm,whitenoise,amplitute,1,0,donde);
+    for a=1:3
+    Dist=table();
+    F0=table();
+
+        for i=1:Sims
+        %Simulating
+            [TFFT,dur,best_cand,distaway,found,f0]=experiment_multi_sigs(Num(n),minfreq(i),maxfreq(i),chirpm(i),whitenoise,amplitute(a),0,0,donde);
     
-    %Filling output array with output vals
-    registered_sig(i)=found;
-    % tfft(i)=TFFTmax;
-    
+        %Filling output array with output vals
+            registered_sig(i)=sum(found);
+            tfft(i)=TFFT;
+            Dur(i)=dur;
+            Cr(i)=best_cand(6);
+            %DistAway(i)=distaway;
+            %F0(i)=f0;
+            amp(i)=amplitute(a);
+           
+            
+            %Table Variables
+
+            f0_temp=table(f0);
+
+            F0=[F0;f0_temp];
+
+            dist_temp = table(distaway) ;
+            Dist = [Dist ; dist_temp];
+   
+        end
+        %Storing everything into a df
+        Sim_df(Sims,Num(n),registered_sig,tfft,F0,Dist,Dur,Cr,chirpm,minfreq,maxfreq,amp)
+
+    end
+
 end
 
-%Storing everything into a df
-Sim_df(10,Num,registered_sig,tfft,minffreq,maxfreq,amplitute,chirpm)
+
+
+
 
 
--- Sim_df.m
+++ Sim_df.m
@@ -1,4 +1,4 @@
-function [df] =Sim_df(Num_sims, n,Retireved_sig,tfft,minfreq,maxfreq,amps,chirpm)
+function [df] =Sim_df(Num_sims,n,Retireved_sig,tfft,Fo,Dist,Durration,cr,chirpm,minfreq,maxfreq,amps)
 
 %Sim_df
 %This function creates and stores a dafa frame of a hough simulation
@@ -18,33 +18,56 @@ function [df] =Sim_df(Num_sims, n,Retireved_sig,tfft,minfreq,maxfreq,amps,chirpm
 
  N=zeros(Num_sims,1);
  Retireved_N=zeros(Num_sims,1); 
- TFFT=zeros(Num_sims,1);
- minf=zeros(Num_sims,1);
- maxf=zeros(Num_sims,1);
- amp=zeros(Num_sims,1);
- cm=zeros(Num_sims,1);
-
-
-
+ TFFT=zeros(Num_sims,1);               %lengths of time of Foyer transmform 
+ minf=zeros(Num_sims,1);               %sim input
+ maxf=zeros(Num_sims,1);               %sim input
+ h0=zeros(Num_sims,1);                 %amplitude
+ cm=zeros(Num_sims,1);                 %sim input
+ f0=zeros(Num_sims,1);                 %frequency at given points (this will be a nested array in each entry)
+ dur=zeros(Num_sims,1);                % durration of time signal is ditected
+ Cr=zeros(Num_sims,1);                 %critical ratio
+ %distaway=zeros(Num_sims,1);
 
 %Insertinng values into empty arrays
 for i = 1:Num_sims
-    N(i)=n(i);
+    N(:)=n;
     Retireved_N(i)=Retireved_sig(i); 
     TFFT(i)=tfft(i);
     minf(i)= minfreq(i);
-    maxf(i)=maxfreq(i); 
-    amp(i)=amps(i);
+    maxf(i)=maxfreq(i);
+    Cr(i)=cr(i);
     cm(i)=chirpm(i);
-
+    h0(i)=amps(i);
+    dur(i)=Durration(i);
+    %f0(i)=Fo(i);  
+    %distaway(i)=Dist(i);
 
 end
 
 
 %Composing tabel of values
-df=table(N,Retireved_N,TFFT,minf,maxf,amp,cm);
+
+df=table(N,Retireved_N,TFFT,dur,Cr,cm,minf,maxf,h0);
+
+%Merging tables into one large dataframe
+df=[df,Dist];
+df=[df,Fo];
+
+
+N1=N(1);
+h01=h0(1);
+
+%Naming the datafile
+filename= append('df_',num2str(N1),"Sig_Amp:",num2str(h01));
 
 %Saving df as csv file 
-writetable(df,'df.csv');
+writetable(df,append(filename,'.csv'));
+
 
 end
+
+
+
+
+
+
